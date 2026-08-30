537cca90623ed9b12e7d1e00fb17b791be724448
xiongzihong <734604253@qq.com>
2026-08-30 12:51:56 +0800

    Deploy fitness training mobile PWA

---
+++ mobile/sw.js
@@ -0,0 +1,32 @@
+const CACHE = "fitness-diet-mobile-v4";
+const APP_SHELL = ["/mobile/", "/mobile/manifest.webmanifest", "/mobile/icon.svg", "/mobile/icon-192.png", "/mobile/icon-512.png"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))).then(() => self.clients.claim()));
+});
+
+self.addEventListener("fetch", (event) => {
+  const request = event.request;
+  if (request.method !== "GET") return;
+  const url = new URL(request.url);
+  if (url.pathname.startsWith("/api/")) return;
+  if (request.mode === "navigate") {
+    event.respondWith(fetch(request).then((response) => {
+      const copy = response.clone();
+      caches.open(CACHE).then((cache) => cache.put("/mobile/", copy));
+      return response;
+    }).catch(() => caches.match("/mobile/")));
+    return;
+  }
+  event.respondWith(caches.match(request).then((cached) => cached || fetch(request).then((response) => {
+    if (response.ok && url.origin === self.location.origin) {
+      const copy = response.clone();
+      caches.open(CACHE).then((cache) => cache.put(request, copy));
+    }
+    return response;
+  })));
+});
